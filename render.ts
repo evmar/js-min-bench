f43bf80aaff67700d35b590ccad72682c57b38fa
Evan Martin <evan.martin@gmail.com>
2017-12-14 05:49:49 -0800

    formatting

--- render.ts
+++ render.ts
@@ -24,7 +24,7 @@ function minRow<T, K extends keyof T>(data: T[], key: K): T {
 }
 
 function percent(n: number, total: number): string {
-    return (n * 100 / total).toFixed(1) + '%';
+    return (n * 100 / total).toFixed(0) + '%';
 }
 
 function main() {
@@ -41,15 +41,22 @@ function main() {
         let bestTime = minRow(results.slice(1), 'time');
         for (let result of results) {
             html += `<tr><td>${result.tool}</td>`;
+
             let best = result === bestSize ? ' class=best' : '';
             html += `<td align=right${best}>${result.size.toLocaleString()}</td>`
             html += `<td align=right${best}>${percent(result.size, baseline.size)}</td>`;
+
             best = result === bestGz ? ' class=best' : '';
             html += `<td align=right${best}>${result.gzSize.toLocaleString()}</td>`;
             html += `<td align=right${best}>${percent(result.size, baseline.size)}</td>`;
-            best = result === bestTime ? ' class=best' : '';
-            let time = (result.time / 1000).toFixed(1);
-            html += `<td align=right${best}>${time}</td></tr>`;
+
+            if (result === baseline) {
+                html += `<td></td>`;
+            } else {
+                best = result === bestTime ? ' class=best' : '';
+                let time = (result.time / 1000).toFixed(1);
+                html += `<td align=right${best}>${time}</td></tr>`;
+            }
         }
         html += `</table>`;
         html += `</p>`;
