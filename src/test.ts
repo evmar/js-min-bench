4913e0610150aff83215b584c0a0089282f2f800
Evan Martin <evan.martin@gmail.com>
2019-04-19 19:21:36 -0700

    test complete all

--- src/test.ts
+++ src/test.ts
@@ -10,6 +10,16 @@ async function getItems(page: puppeteer.Page) {
   return await page.$$eval('ul.todo-list li', es => es.map(e => e.textContent));
 }
 
+/** Gets the completed state of the displayed todo items. */
+async function getItemsCompleted(page: puppeteer.Page) {
+  return await page.$$eval('ul.todo-list li', es => es.map(e => e.classList.contains('completed')));
+}
+
+/** Gets the toggles of the displayed todo items. */
+async function getItemsToggle(page: puppeteer.Page) {
+  return await page.$$('ul.todo-list li input.toggle');
+}
+
 /** Adds a todo item by typing it in. */
 async function addItem(page: puppeteer.Page, text: string) {
   const input = (await page.$(newInputSelector))!;
@@ -101,6 +111,56 @@ describe('todomvc', function() {
       await addItem(page, 'one');
       await isVisible(page, 'main', true);
       await isVisible(page, 'footer', true);
+    });
+  });
+
+  describe('mark all as completed', function () {
+    async function clickToggleAll() {
+      await page.$eval('input.toggle-all', t => (t as HTMLElement).click());
+    }
+    async function getToggleAllChecked() {
+      return await page.$eval('input.toggle-all', t => (t as HTMLInputElement).checked);
+    }
+
+    beforeEach(async function() {
+      await addItem(page, 'one');
+      await addItem(page, 'two');
+      await addItem(page, 'three');
+    });
+
+    it('marks all', async function () {
+      await clickToggleAll();
+      expect(await getItemsCompleted(page)).eql([true, true, true]);
+    });
+
+    it('should correctly update the complete all checked state', async function () {
+      const toggles = await getItemsToggle(page);
+      await page.evaluate(t => t.click(), toggles[0]);
+      await page.evaluate(t => t.click(), toggles[1]);
+      await page.evaluate(t => t.click(), toggles[2]);
+
+      expect(await getToggleAllChecked()).true;
+    });
+
+    it('should allow me to clear the completion state of all items', async function () {
+      await clickToggleAll();
+      await clickToggleAll();
+      expect(await getItemsCompleted(page)).eql([false, false, false]);
+    });
+
+    it('complete all checkbox should update state when items are completed / cleared', async function () {
+      await clickToggleAll();
+      expect(await getToggleAllChecked()).true;
+
+      // Mark one not complete.
+      const toggles = await getItemsToggle(page);
+      expect(await getToggleAllChecked()).true;
+      await page.evaluate(t => t.click(), toggles[0]);
+      expect(await getToggleAllChecked()).false;
+
+      // Mark as complete, so that once again all items are completed.
+      await page.evaluate(t => t.click(), toggles[0]);
+      expect(await getToggleAllChecked()).true;
     });
   });
 });
